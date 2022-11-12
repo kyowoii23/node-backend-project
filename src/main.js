cb8659b3da7b1d99fcc6a4113f356ee6b3a73dfc
kyowoii23 <{kyowoii23@gmail.com}>
2022-11-12 15:42:06 +0900

    Express 미들웨어 이해하기

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 // @ts-check
+
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+
+const PORT = 4000
+
+app.use('/', async (req, res, next) => {
+  console.log('middleware 1')
+  const requestedAt = new Date()
+  const fileContent = await fs.promises.readFile('.gitignore')
+
+  req.requestedAt = requestedAt
+  req.fileContent = fileContent
+  next()
+})
+
+/* ...middleware */
+
+app.use('/', (req, res, next) => {
+  console.log('middleware 2')
+  res.send(`requested at ${req.requestedAt}, file : ${req.fileContent}`)
+})
+
+app.listen(PORT, () => {
+  console.log(`The Express server is listening at port ${PORT}`)
+})
