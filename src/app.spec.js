b72466c99da6e9a2c17c713fe72b53773220506d
kyowoii23 <{kyowoii23@gmail.com}>
2022-11-21 18:38:12 +0900

    Jest를 활용한 API 테스팅

---
+++ src/app.spec.js
@@ -0,0 +1,33 @@
+/* eslint-disable no undef */
+
+const supertest = require('supertest')
+const app = require('./app')
+
+const request = supertest(app)
+
+test('retrieve user json', async () => {
+  const result = await request.get('/users/15').accept('application/json')
+
+  expect(result.body).toMatchObject({
+    nickname: expect.any(String),
+  })
+})
+
+test('retrieve user page', async () => {
+  const result = await request.get('/users/15').accept('text/html')
+
+  expect(result.text).toMatch(/^<html>.*<\/html>$/)
+})
+
+test('update nickname', async () => {
+  const newNickname = 'newNickname'
+
+  const res = await request.post('/users/15/nickname').send({ nickname: newNickname })
+  expect(res.status).toBe(200)
+
+  const userResult = await request.get('/users/15').accept('application/json')
+  expect(userResult.status).toBe(200)
+  expect(userResult.body).toMatchObject({
+    nickname: newNickname,
+  })
+})
--- src/main.js
+++ src/main.js
@@ -1,24 +1,8 @@
 // @ts-check
 
-const express = require('express')
-
-const app = express()
-app.use(express.json())
-app.set('views', 'src/views')
-app.set('view engine', 'pug')
+const app = require('./app')
 
 const PORT = 4000
-
-const userRouter = require('./routers/user')
-
-app.use('/users', userRouter)
-app.use('/public', express.static('src/public'))
-
-// error handling middleware
-app.use((err, req, res, next) => {
-  res.statusCode = err.statusCode || 500
-  res.send(err.message)
-})
 
 app.listen(PORT, () => {
   console.log(`The Express server is listening at port ${PORT}`)
